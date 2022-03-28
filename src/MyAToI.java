4c967003f69d6b6a98d946094b9bc4344e7d3442
Sri Krishna J <srikrishna11@protonmail.ch>
2022-03-28 08:45:45 +0530

    MyAToI - optimised - reduced condition checks

--- src/MyAToI.java
+++ src/MyAToI.java
@@ -8,18 +8,19 @@ public class MyAToI {
     private static boolean isSpecial(char input){
         return input == ' ' ;
     }
+    private static int overFLowValue(boolean negative){
+        return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+    }
     public static void main(String[] args) {
 
-        long max = Integer.MIN_VALUE;
-
-        String s = "18446744073709551617";
+        String s = "-18446744+073709551617";
+        s = s.trim();
         System.out.println(Long.MAX_VALUE);
 
 
         boolean numberStarted = false;
         boolean negative = false;
         boolean positive = false;
-        boolean flipped = false;
 
         long number = 0;
         int length = s.length();
@@ -30,44 +31,27 @@ public class MyAToI {
                 int digit = c - '0';
                 if(digit==-3 && !numberStarted){
                     if(positive || negative) break;
-                    negative = !negative;
+                    negative = true;
                 }else if(digit == -5 && !numberStarted){
                     if(negative || positive) break;
                     positive = true;
-
                 }else if(isSpecial(c)) break;
                 else if(digit>-1){
                     numberStarted = true;
                     long tens = number*10;
-                    if(tens < 0){
-                        System.out.println("Sudden flip!");
-                        number = Integer.MAX_VALUE;
-                        flipped = true;
-                        break;
-                    }
                     number = tens+ digit;
-                    if(number < 0){
-                        System.out.println("sudden flip!");
-                        number = Integer.MAX_VALUE;
-                        flipped = true;
-                        break;
+                    if(tens < 0 || number<0){
+                        System.out.println("Sudden flip!");
+                        number = overFLowValue(negative);
+                        break; //return
                     }
                 }else break;
             }else if(!numberStarted && !isSpecial(c)){ negative=false; break;}
             else if(numberStarted && isSpecial(c)) break;
-            else if(isSpecial(c) && !positive && !negative) {}
             else break;
         }
 
-        if(!negative && number < 0 ) number =  Integer.MAX_VALUE;
-        else if(!negative && flipped) number = Integer.MAX_VALUE;
-        else if( negative && number < Integer.MIN_VALUE) number = Integer.MIN_VALUE;
-        else if(negative && number > Integer.MAX_VALUE) number = Integer.MIN_VALUE;
-        else if(number > (long) Integer.MAX_VALUE) number = Integer.MAX_VALUE;
-        else if ( negative && number <= Integer.MAX_VALUE && !flipped) number = number*-1;
-        else if(negative && number <= Integer.MAX_VALUE && flipped) number = Integer.MIN_VALUE;
-
-
+        if(negative) number = number*-1;
         System.out.println(number);
     }
 }
